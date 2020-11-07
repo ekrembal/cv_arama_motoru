64104250125798da605f1d15dc1fc60edc33c71b
Ekrem BAL <mail.ekrembal@gmail.com>
2020-11-08 00:37:57 +0300

    ui improvements

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response
 
 from ner import extract_entities
 from pdf_operations import get_text_from_pdf
@@ -14,7 +14,22 @@ def get_cv_texts():
         if file.endswith(".pdf"):
             filename = os.path.join("cv_ler", file)
             cv_texts.append((filename, get_text_from_pdf(filename)))
+            print(cv_texts[-1], '\n\n\n')
 
+
+
+@api.route("/cv_goruntule", methods=['POST', 'GET'])
+def cv_goruntule():
+    filename = request.args.get('cv')
+    if id is not None:
+        print(os.path.join('cv_ler', filename))
+        binary_pdf = open(os.path.join('cv_ler', filename), 'rb').read()
+        print(binary_pdf)
+        response = make_response(binary_pdf)
+        response.headers['Content-Type'] = 'application/pdf'
+        response.headers['Content-Disposition'] = \
+            'inline; filename=%s.pdf' % 'yourfilename'
+        return response
 
 @api.route("/search", methods=['POST', 'GET'])
 def search():
@@ -26,11 +41,18 @@ def search():
     cv_scores = []
     for cv_name, cv_text in cv_texts:
         score = 0
+        found_entities = []
+        cant_found_entities = []
         for entity in entities:
             if(entity.lower() in cv_text):
                 score += 1
-        cv_scores.append(( int(100*score/len(entities)), cv_name))
-    return render_template('result.html', cv_scores=cv_scores)
+                found_entities.append(entity)
+            else:
+                cant_found_entities.append(entity)
+        cv_scores.append(( int(100*score/len(entities)), cv_name, found_entities, cant_found_entities))
+        # print(found_entities)
+    cv_scores = reversed(sorted(cv_scores))
+    return render_template('result.html', cv_scores=cv_scores, soru=question)
 
 @api.route('/')
 def main_page():
